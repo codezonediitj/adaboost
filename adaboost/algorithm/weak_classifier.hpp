7b31ca4a9848a7aa392c47cbe1d0fa8b56cdeab0
czgdp1807 <gdp.1807@gmail.com>
2020-12-05 16:53:28 +0530

    Documentation Done

--- adaboost/algorithm/weak_classifier.hpp
+++ adaboost/algorithm/weak_classifier.hpp
@@ -21,7 +21,7 @@ namespace adaboost
         /*
         * Interface for all the binary weak classifiers.
         *
-        * @tparam data_type_vector Data type supported by C++.
+        * @tparam data_type Data type supported by C++.
         */
         template <class data_type>
         class BinaryWeakClassifier : public Base
@@ -46,7 +46,7 @@ namespace adaboost
                                         Vector<data_type>* example_weights,
                                         data_type precision) = 0;
 
-                /*
+                /*@overload
                 * This method should be overrided in specific
                 * weak classifiers to implement their prediction
                 * algorithms.
@@ -69,23 +69,45 @@ namespace adaboost
 
         };
 
+        /*
+        * Factory for creating binary weak classifiers.
+        *
+        * @tparam data_type Data type supported by C++.
+        */
         template <class data_type>
         class BinaryWeakClassifierFactory: public Base
         {
 
             public:
 
+                /*
+                * This method creates the factory object.
+                */
                 static BinaryWeakClassifierFactory*
                 create_BinaryWeakClassifierFactory();
 
+                /*@overload
+                * This method creates the given type of binary
+                * weak classifier according to the classifier
+                * information.
+                *
+                * @param classifier_information Properties* The classifier information.
+                * @param classifier_type std::string The type of weak classifier.
+                */
                 virtual BinaryWeakClassifier<data_type>* create_BinaryWeakClassifier
                 (Properties* classifier_information,
                  std::string classifier_type);
 
+                /*
+                * Frees the memory occupied by the factory object.
+                */
                 virtual ~BinaryWeakClassifierFactory();
 
             protected:
 
+                /*
+                * Default constructor.
+                */
                 BinaryWeakClassifierFactory();
 
         };
